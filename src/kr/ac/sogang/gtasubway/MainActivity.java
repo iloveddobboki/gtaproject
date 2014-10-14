e3060d9de12f341bc61debd7ca4e7017b673510c
minhoryang <minhoryang@gmail.com>
2014-10-14 21:30:40 +0900

    안녕하세요 민호입니다. 멘토입니다.

--- src/kr/ac/sogang/gtasubway/MainActivity.java
+++ src/kr/ac/sogang/gtasubway/MainActivity.java
@@ -18,7 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
+// Hello World! by Minho Ryang.
 public class MainActivity extends Activity implements ActionBar.TabListener {
 
     /**
