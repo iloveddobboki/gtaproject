923193c663da215ec69433e562d43f13d6019959
yujinee <dhaldhd@naver.com>
2014-10-23 21:19:36 +0900

    1.최단시간 버튼누르면 내용이 바뀌어요 2.역명 버튼을 누르면 (역무원에게 전화걸기/역사진/텍스트)가 있는 엑티비틱로 전환되어요 라라라라라라라랄라

--- src/kr/ac/sogang/gtasubway/search/SearchedSubway.java
+++ src/kr/ac/sogang/gtasubway/search/SearchedSubway.java
@@ -2,32 +2,38 @@ package kr.ac.sogang.gtasubway.search;
 
 import kr.ac.sogang.gtasubway.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.Time;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class SearchedSubway extends Activity {
-
+		StationInfo shinchon = new StationInfo("신촌", 2, 31, 1, 1, new Time()/*어쩔*/, new Time()), ahyen = new StationInfo("아현", 2, 31, 2, 2, new Time(), new Time());
+		
+		SearchedSubwayInfo searchedSubway=new SearchedSubwayInfo(shinchon, ahyen, 1000, 10, 2);
+		TextView tvLineStation1, tvDoorStation1, tvWidth1, tvHeight1, tvLineStation2, tvDoorStation2, tvWidth2, tvHeight2, tvArrivalTime, tvDepartureTime, tvTime, tvNumOfStations, tvFare;
+		Button btDeparture, btDestination, btSearchMode;
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_searched_subway);
 		
-		StationInfo shinchon = new StationInfo("신촌", 2, 31, 1, 1, new Time(), new Time()), ahyen = new StationInfo("아현", 2, 31, 2, 2, new Time(), new Time());
-	
-		SearchedSubwayInfo searchedSubway=new SearchedSubwayInfo(shinchon, ahyen, 1000, 10, 2);
+		
 		
 		setTitle(searchedSubway.departure.stationName+"에서"+searchedSubway.destination.stationName+"으로");
+		/*버튼 텍스트 입력 : 역명*/
+		btDeparture=(Button)findViewById(R.id.station1);
+		btDestination=(Button)findViewById(R.id.station2);
+		btSearchMode = (Button)findViewById(R.id.search_mode);
 		
-		Button btDeparture=(Button)findViewById(R.id.station1), btDestination=(Button)findViewById(R.id.station2);
 		
-		btDeparture.setText(searchedSubway.departure.stationName);
-		btDestination.setText(searchedSubway.destination.stationName);
-		
-		TextView tvLineStation1, tvDoorStation1, tvWidth1, tvHeight1, tvLineStation2, tvDoorStation2, tvWidth2, tvHeight2, tvArrivalTime, tvDepartureTime, tvTime, tvNumOfStations, tvFare;
+		/*텍스트뷰 텍스트 입력 : 호선, 승강장번호,단차, 격차,도착시간, 출발시간, 소요시간, 정차역수, 요금*/
 		
 		tvLineStation1 = (TextView)findViewById(R.id.line_station1);
 		tvDoorStation1 = (TextView)findViewById(R.id.door_station1);
@@ -43,6 +49,46 @@ public class SearchedSubway extends Activity {
 		tvNumOfStations = (TextView)findViewById(R.id.numofstations);
 		tvFare = (TextView)findViewById(R.id.fare);
 		
+		setTextAll();
+		
+		btDeparture.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent itDeparture = new Intent(getApplicationContext(), StationOnFoot.class);
+				startActivity(itDeparture);
+			}
+		});
+		
+		btDestination.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent itDestinatione = new Intent(getApplicationContext(), StationOnFoot.class);
+				startActivity(itDestinatione);
+			}
+		});
+
+		btSearchMode.setOnClickListener(new OnClickListener() {
+		
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				searchedSubway=new SearchedSubwayInfo(shinchon, shinchon, 1500, 5, 1);
+				setTextAll();
+				
+			}
+		});
+		
+		
+	}
+	
+	void setTextAll(){
+		btDeparture.setText(searchedSubway.departure.stationName);
+		btDestination.setText(searchedSubway.destination.stationName);
+
 		tvLineStation1.setText(""+searchedSubway.departure.line);
 		tvDoorStation1.setText(""+searchedSubway.departure.door);
 		tvWidth1.setText(""+searchedSubway.departure.width);
@@ -56,26 +102,5 @@ public class SearchedSubway extends Activity {
 		tvTime.setText(""+searchedSubway.time);
 		tvNumOfStations.setText(""+searchedSubway.numOfStations);
 		tvFare.setText(""+searchedSubway.fare);
-		
-		
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.searched_subway, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 }
---
+++ src/kr/ac/sogang/gtasubway/search/StationOnFoot.java
@@ -0,0 +1,42 @@
+package kr.ac.sogang.gtasubway.search;
+
+import kr.ac.sogang.gtasubway.R;
+import kr.ac.sogang.gtasubway.R.id;
+import kr.ac.sogang.gtasubway.R.layout;
+import kr.ac.sogang.gtasubway.R.menu;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+public class StationOnFoot extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_station_on_foot);
+		WebView webView1 = (WebView)findViewById(R.id.webView1);
+		webView1.loadUrl("file:///android_asset/file.jpg");
+		webView1.getSettings().setBuiltInZoomControls(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.station_on_foot, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
