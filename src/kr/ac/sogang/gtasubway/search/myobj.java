e5eb7ac92db2aaf1eaf4cd681da0940209c04697
정유진 <@yujinjung-pc>
2014-10-26 19:54:05 +0900

    ?

---
+++ src/kr/ac/sogang/gtasubway/search/myobj.java
@@ -0,0 +1,15 @@
+package kr.ac.sogang.gtasubway.search;
+
+import java.io.Serializable;
+
+public class myobj implements Serializable{
+
+	int i, j;
+	String k;
+	
+	public myobj(int i, int j, String k){
+		this.i=i;
+		this.j=j;
+		this.k=k;
+	}
+}
