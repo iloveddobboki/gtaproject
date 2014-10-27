061feb43f4b552bb7e30830e5dae461d3b1f1660
정유진 <@yujinjung-pc>
2014-10-28 01:07:45 +0900

    station class(역의 고정된 정도) 수정 : int stationId 추가

--- src/kr/ac/sogang/gtasubway/search/Station.java
+++ src/kr/ac/sogang/gtasubway/search/Station.java
@@ -12,13 +12,15 @@ public class Station implements Serializable{
 	public String mStation;
 	public String mLine1;
 	public String mLine2;
+	public int stationId;
 	
-	public Station(String mIcon,String map, String mStation, String mLine1, String mLine2){
+	public Station(String mIcon,String map, String mStation, String mLine1, String mLine2, int stationId){
 		this.mIcon=mIcon;
 		this.map=map;
 		this.mStation=mStation;
 		this.mLine1=mLine1;
 		this.mLine2=mLine2;
+		this.stationId=stationId;
 	}
 	public Station(Station station){
 		this.mIcon=station.mIcon;
@@ -26,5 +28,6 @@ public class Station implements Serializable{
 		this.mStation=station.mStation;
 		this.mLine1=station.mLine1;
 		this.mLine2=station.mLine2;
+		this.stationId=station.stationId;
 	}
 }
